e514a41d09cac75490aad5631b10fc57bdf391a9
[dyad] <contato@plugaia.com.br>
2025-09-19 15:26:41 -0300

    [dyad] Removendo a página inicial e redirecionando para o login - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "@/hooks/useAuth";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Layout } from "./components/Layout";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
-import Landing from "./pages/Landing";
 import ProposalView from "./pages/ProposalView";
 import Configuracoes from "./pages/Configuracoes";
 import Clientes from "./pages/Clientes";
@@ -61,7 +60,7 @@ const AppRoutes = () => {
               </Layout>
             </ProtectedRoute>
           ) : (
-            <Landing />
+            <Navigate to="/auth" replace />
           )
         } />
         <Route path="/configuracoes" element={
